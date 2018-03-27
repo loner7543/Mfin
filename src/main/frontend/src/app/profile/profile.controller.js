2bff6a026dde7e8de09a292f891e9002c782ced9
loner7543 <Sachok1394@mail.ru>
2018-03-28 00:50:35 +0400

    Рисование осей

--- src/main/frontend/src/app/profile/profile.controller.js
+++ src/main/frontend/src/app/profile/profile.controller.js
@@ -15,34 +15,34 @@
     };
 
     $scope.calcProfile=function () {
-      debugger;
       console.log($scope.profileParams.fileName)
       $http({
         method: "POST",
         url: "http://localhost:8080/krugstat/rest/calculateProfile",
         params:$scope.profileParams
       }).then(function (resp) {
-          console.log("Профиль", resp)
+          console.log("Профиль", resp);
           $scope.profile=resp.data;
         },
         function (result) {
+          UtilsFunctionsFactory.
           console.error(result, result.data);
         });
     };
 
     $scope.calculateCruglogramme = function () {
-      $scope.drawCircle();
       console.log($scope.profileParams.mode);
       $http({
         method: "POST",
         url: "http://localhost:8080/krugstat/rest/calculateCluglogramme",
         params:$scope.profileParams
       }).then(function (resp) {
-          console.log("высоты круглограммы", resp)
+          console.log("высоты круглограммы", resp);
           $scope.cruglogrammeHeights=resp.data;
+          $scope.drawCircle();
         },
         function (result) {
-          console.error(result, result.data);
+          UtilsFunctionsFactory.showFlashMessage('danger',"Высоты не расчитаны");
         });
     };
 
@@ -52,21 +52,61 @@
     $scope.drawCircle = function () {
       var canvas = document.getElementById("circleCanvas");
       var ctx = canvas.getContext("2d");
-
       debugger;
-      $http({
-        method: "POST",
-        url: "http://localhost:8080/krugstat/rest/getCirclePoints",
-        params:{
-          radius:1
+      var Xpoints = [];
+      Xpoints.push({
+        x:0,
+        y:canvas.height/2
+      });
+      for (var i = 0;i<canvas.width;i++){
+        if (i%3===0){
+          Xpoints.push({
+            x:i,
+            y:canvas.height/2
+          })
         }
-      }).then(function (resp) {
-          console.log("Точки", resp);
-          $scope.cruglogrammeHeights=resp.data;
-        },
-        function (result) {
-          console.error(result, result.data);
-        });
+      }
+
+      var Ypoints = [];
+      Ypoints.push({
+        x:canvas.width/2,
+        y:0
+      });
+      for (var j = 0;i<canvas.height;j++){
+        if (j%3===0){
+          Ypoints.push({
+            x:canvas.width/2,
+            y:j
+          })
+        }
+      }
+      //draw axis
+      ctx.beginPath();
+      for(var k = 1; k<Xpoints.length; k++)
+      {
+        if((k%2)!==0){
+          ctx.moveTo(Xpoints[k-1].x,Xpoints[k-1].y);
+          ctx.lineTo(Xpoints[k].x,Xpoints[k].y);
+        }
+      }
+      ctx.stroke();
+      console.log("Done!");
+
+
+      // $http({
+      //   method: "POST",
+      //   url: "http://localhost:8080/krugstat/rest/getCirclePoints",
+      //   params:{
+      //     radius:1
+      //   }
+      // }).then(function (resp) {
+      //     console.log("Точки", resp);
+      //     $scope.cruglogrammeHeights=resp.data;
+      //
+      //   },
+      //   function (result) {
+      //     console.error(result, result.data);
+      //   });
     };
   }
 })();
